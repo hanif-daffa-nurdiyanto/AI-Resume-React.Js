fc29785eb4d4e039370d5e7013d99aa2d41d78f5
Hanif Daffa N <mail.hanifdaffan@gmail.com>
2025-07-18 10:28:05 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const HOST = `${env.VITE_HOST ?? "http://localhost:3000"}`;
+  const PORT = `${env.VITE_PORT ?? "3000"}`;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: HOST,
+      port: PORT,
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
